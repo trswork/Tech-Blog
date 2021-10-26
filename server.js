67ed228a138c39c5eda129a0cc4a0167cd35cde4
trswork <87744214+trswork@users.noreply.github.com>
2021-10-25 21:00:24 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, () => {
+    console.log('App listening on PORT ' + PORT);
+  });
